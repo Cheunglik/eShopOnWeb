c7f38d6074435552663572ae88204950e248af9a
Cheunglik <juanlik@msn.com>
2023-09-08 22:18:56 +0800

    test inject IHttpContextAccessor in CatalogContext

--- src/Infrastructure/Data/CatalogContext.cs
+++ src/Infrastructure/Data/CatalogContext.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
 using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
@@ -9,7 +12,10 @@ namespace Microsoft.eShopWeb.Infrastructure.Data;
 public class CatalogContext : DbContext
 {
     #pragma warning disable CS8618 // Required by Entity Framework
-    public CatalogContext(DbContextOptions<CatalogContext> options) : base(options) {}
+    public CatalogContext(DbContextOptions<CatalogContext> options, IHttpContextAccessor httpContextAccessor) : base(options) 
+    {
+        var name = httpContextAccessor.HttpContext?.User.Claims.SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value ?? "";
+    }
 
     public DbSet<Basket> Baskets { get; set; }
     public DbSet<CatalogItem> CatalogItems { get; set; }
--- src/Infrastructure/Data/EfRepository.cs
+++ src/Infrastructure/Data/EfRepository.cs
@@ -1,11 +1,15 @@
 ﻿using Ardalis.Specification.EntityFrameworkCore;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Security.Claims;
 
 namespace Microsoft.eShopWeb.Infrastructure.Data;
 
 public class EfRepository<T> : RepositoryBase<T>, IReadRepository<T>, IRepository<T> where T : class, IAggregateRoot
 {
-    public EfRepository(CatalogContext dbContext) : base(dbContext)
+    public EfRepository(CatalogContext dbContext, IHttpContextAccessor httpContextAccessor) : base(dbContext)
     {
+        var name = httpContextAccessor.HttpContext?.User.Claims.SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value ?? "";
     }
 }
